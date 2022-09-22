f1184e32621b6d81ba266885eeff8bfce0209aa3
KOKKERA SIVASANKAR <112496249+sivasankarkokkera@users.noreply.github.com>
2022-09-22 10:57:02 +0530

    Add files via upload

---
+++ permutations 7.py
@@ -0,0 +1,8 @@
+from itertools import permutations
+list1=[]
+m=int(input("enter the number of values to be entered in a list"))
+for i in range(0,m):
+    h=int(input())
+    list1.append(h)
+l = list(permutations(list1))
+print(l)
